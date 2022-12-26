d5486c5f1b39786388efc339f064e7bec99948a8
aishibata6 <ai.shibata1@gmail.com>
2022-12-25 19:51:06 -0800

    helper function to avoid repeat code

--- script.js
+++ script.js
@@ -9,20 +9,32 @@ const count = 10;
 const apiKey = 'chx4wgQdU8nTAvfE1SVPTm7kqA7tihPVTh_rB8MD0u8';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
+// helper function to avoid repeat code 
+function setAttributes(element, attributes) {
+    // key = href, target, src, alt
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    } 
+}
+
 // create elements for links & photos, add to DOM
 function displayPhotos() {
     // run function for each object in photosArray
     photosArray.forEach((photo) => {
         // create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
         console.log(item);
         //create <img> for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
 
         //put <img> inside <a>, then put that <img> inside image container
         item.appendChild(img);
